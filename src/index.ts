cc880041a956ec2fc2acb665f656f20f5cec425f
sadvos <application77863@gmail.com>
2022-08-06 09:44:27 +0100

    feat: ✨ Adds a WebSocket

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('Hello World');
+const http = require('http');
+const express = require('express');
+const webSocket = require('ws');
+
+const app = express()
+let port = 8080;
+
+const server = http.createServer(app);
+const webSocketServer = new webSocket.Server({ server: server });
+
+webSocketServer.on('connection', (webSocket: any) => {
+    webSocket.send('Connected to the webSocket server');
+
+    webSocket.on('message', function incoming(message: any) {
+        webSocketServer.clients.forEach((client: any) => {
+            // Filter the JSON data here
+            if (client !== webSocket && client.readyState === webSocket.OPEN) {
+                // Send a response based on the JSON data
+                client.send('this is a test and will in the future be a response')
+            }
+        })
+    })
+
+})
+
+app.get('/', (req: any, res: any) => res.send('Hello World!'))
+
+server.listen(port, () => console.log(`Server listening on port ${port}`))
