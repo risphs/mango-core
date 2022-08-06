b5e21a5cbce3330e1bf84f2cfa0167f214afd006
sadvos <application77863@gmail.com>
2022-08-06 13:18:41 +0100

    feat: ✨ Adds checks for incoming and outgoing responses

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@ let port: Number = 8000;
 const server = http.createServer(app);
 const webSocketServer = new webSocket.Server({ server: server });
 
+let response: String = null; // Responses will be strigified JSON objects
+
+interface socketResponseLayout {
+    header: String,
+    body: String
+}
+
 webSocketServer.on('connection', (webSocket: any) => {
     webSocket.send('Connected to the WebSocket server');
 
-    webSocket.on('message', function incoming(message: any) {
+    webSocket.on('message', function incoming(message: string) { // Incoming
         webSocketServer.clients.forEach((client: any) => {
-            // Filter the JSON data here
+            incomingChecks(JSON.parse(message));
             if (client !== webSocket && client.readyState === webSocket.OPEN) {
                 // Send a response based on the JSON data
                 client.send('this is a test and will in the future be a response')
@@ -43,9 +50,59 @@ socket.addEventListener('error', function (event: any) {
 });
 
 socket.addEventListener('message', function (event: any) {
-    console.log("Server Sent: ", event.data);
+    console.log(event.data);
 });
 
-const sendMessage = (message: any) => {
+const sendMessage = (message: string) => {
     socket.send(message);
+}
+
+/* Ideas for the future
+SYSTEM-VIEW-LOGS ~ KEEP IN MIND IDEA OF LOGS
+*/
+
+
+
+export function incomingChecks(input: socketResponseLayout) {
+    if(input.header === 'CONSOLE-RUN') {
+
+    } else if(input.header === 'CONSOLE-GET-TEXT') {
+
+    } else if(input.header === 'SYSTEM-TOTAL-MEMORY') {
+
+    } else if(input.header === 'SYSTEM-USED-MEMORY') {
+
+    } else if(input.header === 'SYSTEM-REGISTERED-PROGRAM') {
+
+    } else if(input.header === 'SYSTEM-RESTART') {
+
+    } else if(input.header === 'SYSTEM-SHUTDOWN') {
+
+    } else if(input.header === 'CURRENT-INCOMING-TRAFFIC') {
+
+    } else if(input.header === 'CURRENT-MEMORY-USAGE') {
+
+    } else if(input.header === 'CURRENT-CPU-USAGE') {
+
+    }
+}
+
+export function outgoingChecks(input: socketResponseLayout) {
+    if(input.header === 'CONSOLE-INFORMATION') {
+
+    } else if(input.header === 'CONSOLE-ERROR') {
+
+    } else if(input.header === 'SYSTEM-TOTAL-MEMORY-NUMBER') {
+
+    } else if(input.header === 'SYSTEM-USED-MEMORY-NUMBER') {
+
+    } else if(input.header === 'SYSTEM-REGISTERED-PROGRAM-INFORMATION') {
+
+    } else if(input.header === 'CURRENT-INCOMING-TRAFFIC-NUMBER') {
+
+    } else if(input.header === 'CURRENT-MEMORY-USAGE-NUMBER') {
+
+    } else if(input.header === 'CURRENT-CPU-USAGE-NUMBER') {
+
+    }
 }
